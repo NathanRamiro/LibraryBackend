69f2cd34f89e3003283c91743ceb4c0d0d68b731
Nathan Ramiro <nathan.ramiro1912@gmail.com>
2024-07-12 22:01:24 -0300

    GenreControllerTest Cleanup

--- src/test/java/com/nathanramiro/springtest/genre/GenreControllerTest.java
+++ src/test/java/com/nathanramiro/springtest/genre/GenreControllerTest.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,21 +25,22 @@ public class GenreControllerTest {
     JdbcGenreRepository repository;
 
     private final List<String> genres = new ArrayList<>();
+    private final String basePath = "/api/genres";
 
     @BeforeEach
-    void setup(){
+    void setup() {
         genres.clear();
     }
-    
+
     @Test
     void testGetAll() throws Exception {
         genres.add("genreName2");
         genres.add("genreName3");
         genres.add("genreName");
 
-        when(repository.getAll()).thenReturn(genres);
-        mvc.perform(get("/api/genres"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.size()",is(genres.size())));
+        when(repository.getAll()).thenReturn(genres); // testing like this feels pointless
+        mvc.perform(get(basePath))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(genres.size())));
     }
 }
