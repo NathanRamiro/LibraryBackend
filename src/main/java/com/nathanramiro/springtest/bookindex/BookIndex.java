dace93c558dedf5c28d8bcdd62f6264d37530b22
Nathan Ramiro <nathan.ramiro1912@gmail.com>
2024-08-21 06:18:12 -0300

    added size restraint to BookIndex.isbn

--- src/main/java/com/nathanramiro/springtest/bookindex/BookIndex.java
+++ src/main/java/com/nathanramiro/springtest/bookindex/BookIndex.java
@@ -2,12 +2,14 @@ package com.nathanramiro.springtest.bookindex;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record BookIndex(
     Integer index_id,
     @NotEmpty
     String book_name,
     @NotNull
+    @Size(min = 13, max = 13)
     String isbn,
     @NotNull
     String writer,
