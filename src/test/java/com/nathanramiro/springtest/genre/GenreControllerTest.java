46ece6a5291efbda4d3c52cdf77fb8586307a62b
Nathan Ramiro <nathan.ramiro1912@gmail.com>
2024-08-21 08:11:38 -0300

    expanded GenreController tests

--- src/test/java/com/nathanramiro/springtest/genre/GenreControllerTest.java
+++ src/test/java/com/nathanramiro/springtest/genre/GenreControllerTest.java
@@ -3,6 +3,7 @@ package com.nathanramiro.springtest.genre;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,6 +16,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.nathanramiro.springtest.bookindex.BookIndex;
+
 @WebMvcTest(GenreController.class)
 public class GenreControllerTest {
 
@@ -24,8 +29,13 @@ public class GenreControllerTest {
     @MockBean
     JdbcGenreRepository repository;
 
-    private final List<String> genres = new ArrayList<>();
     private final String basePath = "/api/genres";
+    private final List<String> genres = new ArrayList<>();
+    private final BookIndex bookIndex = new BookIndex(null,
+            "indexNameTest",
+            "1234567890000",
+            "indexWriterTest",
+            "indexPublisherTest");
 
     @BeforeEach
     void setup() {
@@ -38,9 +48,37 @@ public class GenreControllerTest {
         genres.add("genreName3");
         genres.add("genreName");
 
-        when(repository.getAll()).thenReturn(genres); // testing like this feels pointless
+        when(repository.getAll()).thenReturn(genres);
         mvc.perform(get(basePath))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", is(genres.size())));
+    }
+
+    @Test
+    void testPostAddIndexToGenre() throws Exception {
+
+        genres.add("genreName");
+
+        ObjectWriter objWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        mvc.perform(
+                post(basePath + "/create/addto")
+                        .contentType("application/json")
+                        .content(objWriter.writeValueAsString(List.of(new IndexGenreComp(bookIndex, genres)))))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void testPostNewGenre() throws Exception {
+
+        genres.add("genreNameTest");
+
+        ObjectWriter objWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        mvc.perform(
+                post(basePath + "/create/genre")
+                        .contentType("application/json")
+                        .content(objWriter.writeValueAsString(List.of(new Genre(null, genres.get(0))))))
+                .andExpect(status().isCreated());
     }
 }
